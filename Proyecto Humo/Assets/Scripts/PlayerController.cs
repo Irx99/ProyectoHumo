5f8dec465c92cc0002efcb108bf20fb5cc33076f
irx99 <adriang.gtb@gmail.com>
2017-10-13 14:48:45 +0200

    Movimiento basico

---
+++ Proyecto Humo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity = 1;
+	public GameObject ship;
+
+	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+	void Start()
+	{
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate(){
+		playerMovement ();
+		moveWithShip ();
+	}
+
+	private void playerMovement(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.velocity = movement * velocity;	
+	}
+
+	private void moveWithShip(){
+		rb2d.velocity = rb2d.velocity + ship.GetComponent<Rigidbody2D> ().velocity;
+		//ship.GetComponent<Rigidbody2D>.velocity
+	}
+}
---
+++ Proyecto Humo/Assets/Scripts/SpaceShipMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipMovement : MonoBehaviour {
+
+	public float velocity = 0.2f;
+
+	public Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+	public bool upPlate = false;
+
+	private Vector2 movement;
+
+	void OnTriggerStay2D(Collider2D collider){
+
+		Debug.Log ("hola");
+
+		if (upPlate) {
+			movement = new Vector2 (0, 1);
+		} else {
+			movement = new Vector2 (0, -1);
+		}
+
+		rb2d.velocity = movement * velocity;
+	}
+
+	void OnTriggerExit2D(Collider2D collider){
+		rb2d.velocity = new Vector2 (0, 0);
+	}
+}

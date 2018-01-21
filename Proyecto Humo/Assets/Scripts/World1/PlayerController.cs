1d89c9fef3fe2bc47724fa6fcf9e5003234632f6
irx99 <adriang.gtb@gmail.com>
2018-01-21 02:59:10 +0100

    Primera aproximacion al multijugador. Funcionamiento via servidor de Unity

---
+++ Proyecto Humo/Assets/Scripts/World1/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerController : NetworkBehaviour {
+
+	public float velocity = 1;
+	public GameObject ship;
+	public Sprite localPlayer;
+
+	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+	void Start()
+	{
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update(){
+
+		if (!isLocalPlayer)
+        {
+            return;
+        }
+
+		playerMovement ();
+		//moveWithShip ();
+	}
+
+	private void playerMovement(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.velocity = movement * velocity;	
+	}
+
+/*
+	private void moveWithShip(){
+		rb2d.velocity = rb2d.velocity + ship.GetComponent<Rigidbody2D> ().velocity;
+		//ship.GetComponent<Rigidbody2D>.velocity
+	}
+*/
+
+	public override void OnStartLocalPlayer()
+    {
+		//TODO: Aspecto personalizado para cada jugador, al azar o pre-seleccionado
+		this.GetComponent<SpriteRenderer> ().sprite = localPlayer;
+    }
+}
---
+++ Proyecto Humo/Assets/Scripts/World1/SpaceShipController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour {
+
+	public float velocity = 0.2f;
+
+	public GameObject upPlate;
+	public GameObject downPlate;
+
+	private Rigidbody2D spaceShip_rb2d;
+	private Vector2 movement;
+
+	void Start()
+	{
+		spaceShip_rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnTriggerStay2D(Collider2D collider){
+
+		if (collider == upPlate) {
+			movement = new Vector2 (0, 1);
+		}
+		if (collider == downPlate) {
+			movement = new Vector2 (0, -1);
+		}
+
+		spaceShip_rb2d.velocity = movement * velocity;
+	}
+
+	void OnTriggerExit2D(Collider2D collider){
+		spaceShip_rb2d.velocity = new Vector2 (0, 0);
+	}
+}
